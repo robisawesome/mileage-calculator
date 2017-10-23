e6b30fc2aa6614dd3fb9c3830518d6038324c41a
robisawesome <31973669+robisawesome@users.noreply.github.com>
2017-10-22 19:54:08 -0500

    car.py

---
+++ car.py
@@ -0,0 +1,12 @@
+#Author: Rob Hogue
+#Created: 10/22/2017
+##so sorry that you have to read this, im learning
+
+startingloc = str(input("Where did you leave from?"))
+firstdest = str(input("Where did you go first?"))
+nextdest = str(input("Where did you go next?"))
+
+
+
+
+if startingloc == ())
