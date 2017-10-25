42ad4f73e629a487a12c3da8661be318f46290d1
robisawesome <31973669+robisawesome@users.noreply.github.com>
2017-10-25 14:24:48 -0500

    Add files via upload

--- car.py
+++ car.py
@@ -2,14 +2,21 @@
 #Created: 10/22/2017
 ##so sorry that you have to read this, im learning
 
-startingloc = str(input("Where did you leave from?"))
-firstdest = str(input("Where did you go first?"))
-nextdest = str(input("Where did you go next?"))
+def locations str[,]:
+    startingloc = str(input("Where did you leave from?"))
+        if startingloc in["monona","odana","home"]:
+        
+        else print("Please try again.")
+    #check str value against either google maps api, or by checking in startingloc
+    #directory/list 
+    
+    firstdest = str(input("Where did you go first?"))
+    
+    nextdest = str(input("Where did you go next?"))
 
-startingloc = 
 
 
-if startingloc == str(["monona","odana","home"]):
+
     
 
 
